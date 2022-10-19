15147a517afeda48ef108d20a822631f68598e22
paulhemingway <phemingway@ymail.com>
2022-10-18 19:47:03 -0500

    reversed the lists so the chart renders in correct order

--- backend.py
+++ backend.py
@@ -3,18 +3,26 @@ import datetime
 from lxml import etree
 import pygal
 
+def reverseLists():
+    chartOpen.reverse()
+    chartHigh.reverse()
+    chartLow.reverse()
+    chartClose.reverse()
+    chartDates.reverse()
+
 symbol = "GOOGL"
 chartType = "line"
-timeSeries = "TIME_SERIES_DAILY"
+timeSeries = "TIME_SERIES_MONTHLY"
 apiKey = "33451WDSYYNTOXAH"
 
 beginning = "2021-03-22"
-end = "2021-08-16"
+end = "2022-01-16"
 
 chartOpen = []
 chartHigh = []
 chartLow = []
 chartClose = []
+chartDates = []
 
 print(beginning[0:4])
 
@@ -29,24 +37,29 @@ data = r.json()
 
 ## compare dates with datetime
 
-for entry in data["Time Series (Daily)"]:
+for entry in data["Monthly Time Series"]:
     
     entryDate = datetime.datetime(int(entry[0:4]), int(entry[5:7]), int(entry[8:10]))
     if(entryDate >= beginningDate and entryDate <= endDate):
-        entryObject = data["Time Series (Daily)"][entry]
+        entryObject = data["Monthly Time Series"][entry]
 
         chartOpen.append(float(entryObject["1. open"]))
         chartHigh.append(float(entryObject["2. high"]))
         chartLow.append(float(entryObject["3. low"]))
         chartClose.append(float(entryObject["4. close"]))
+        chartDates.append(entry)
 
+
+reverseLists()
 
 line_chart = pygal.Line()
-line_chart.title = 'Stonks'
-line_chart.x_labels = map(str, range(beginningDate.day, endDate.day))
+line_chart.title = 'Stock Data for ' + symbol + ": " + beginning + " to " +  end
+line_chart.x_labels = chartDates
 line_chart.add('Open', chartOpen)
 line_chart.add('High',  chartHigh)
 line_chart.add('Low',      chartLow)
 line_chart.add('Close',  chartClose)
 line_chart.render_in_browser()
         
+
+
