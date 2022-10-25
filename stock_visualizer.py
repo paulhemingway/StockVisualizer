24d05553d6d752451f13997dfabbd54e7f063c65
tjshipp33 <tjshipp@mail.missouri.edu>
2022-10-25 12:18:17 -0500

    done(?)

--- stock_visualizer.py
+++ stock_visualizer.py
@@ -19,28 +19,28 @@ while graph_type != 0:
     menu()
     graph_type = int(input(''))
 
-#def time_series():
-    #print('Select the Time Series of the Chart You Wish to Generate\n')
-    #print('----------------------------------------------------------\n')
-    #print('Select 1 or 2 for Chart Type\n')
-    #print('1. Intraday')
-    #print('2. Daily')
-    #print('3. Weekly')
-    #print('4. Monthly')
+def time_series():
+    print('Select the Time Series of the Chart You Wish to Generate\n')
+    print('----------------------------------------------------------\n')
+    print('Select 1 or 2 for Chart Type\n')
+    print('1. Intraday')
+    print('2. Daily')
+    print('3. Weekly')
+    print('4. Monthly')
 
-##time_series()
-##time_type = int(input(''))
+time_series()
+time_type = int(input(''))
 
-#while time_type != 0:
-    ##if time_type == 1:
-        ##print('Success!')
-    ##elif time_type == 2:
-        ##print('Success!')
-    ##elif time_type == 3:
-        ##print('Success!')
-    ##elif time_type == 4:
-        ##print('You chose', time_type)
-    ##else: print("Please enter a valid input")
+while time_type != 0:
+    if time_type == 1:
+        print('Success!')
+    elif time_type == 2:
+        print('Success!')
+    elif time_type == 3:
+        print('Success!')
+    elif time_type == 4:
+        print('You chose', time_type)
+    else: print("Please enter a valid input")
 
-    ##menu()
-    ##time_type = int(input(''))
+    menu()
+    time_type = int(input(''))
